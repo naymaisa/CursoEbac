0c028c414e6f3938a2b679befc69cf3efe54359d
Nayara <nayaramaisa.stn@gmail.com>
2024-11-05 11:37:23 -0300

    final alteraçoes

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 const form = document.getElementById('form-calculator');
+var campoB = document.getElementById("campoB");
 
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     var campoA = document.getElementById("campoA");
-    var campoB = document.getElementById("campoB");
     const mensagemSucesso = `O numero: ${campoB.value} é maior que o ${campoA.value}`;
 
-    if (Number(campoB.value) < Number(campoA.value)) {
-        alert("B nao pode ser menor que A, por favor coloque outro valor para B");
-    } else {
-        alert(mensagemSucesso);
+    if (Number(campoB.value) > Number(campoA.value)) {
+        const containerMensagemSucesso = document.querySelector('.sucess-message');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+
         campoA.value = '';
         campoB.value = '';
     }
+    else {
+        document.querySelector('.error-message').style.display = 'block';
 
+    }
 });
 
 
-
-
-console.log(form);
-
+campoB.addEventListener('keyup', function (e) {
+    console.log(e.target.value);
+    if (Number(campoB.value) < Number(campoA.value)) {
+        document.querySelector('.error-message').style.display = 'block';
+    } else {
+        document.querySelector('.error-message').style.display = 'none';
+    }
+})
